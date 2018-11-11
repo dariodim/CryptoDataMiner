db5d0a67ec64a9fcc4e0d08e45c20c44074c700f
dariodim <k.n.dimitrakopoulos@gmail.com>
2018-11-11 22:56:20 +0100

    CoinGecko - Update

--- Main.py
+++ Main.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import Libraries.CoinGecko as CoinGecko_API
 
-# Parse list of coins from CoinGecko
-CoinList_CALL = 'https://api.coingecko.com/api/v3/coins/list'
+NumberOfInstruments = 50
+
+# Parse list of coins from CoinGecko by Market Cap (Highest to Lowest)
+CoinList_CALL = 'https://api.coingecko.com/api/v3/coins?order=market_cap_desc?'
 CoinList = pd.DataFrame(CoinGecko_API.CoingeckoAPI(CoinList_CALL).get_coingecko_data())
 
 # Parse full information about bitcoin
-CoinInformation_CALL = 'https://api.coingecko.com//api/v3/coins/bitcoin'
+CoinInformation_CALL = 'https://api.coingecko.com//api/v3/coins/' + CoinList['id'][0]
 CoinInformation = CoinGecko_API.CoingeckoAPI(CoinInformation_CALL).get_coingecko_data()
 
-#CoinInformation = pd.DataFrame(CoinGecko_API.CoingeckoAPI(CoinInformation_CALL).get_coingecko_data())
-print(type(CoinInformation))
+
+
+# Id of instrument
+Id = CoinInformation['id']
+
+# Name of instrument
+Name = CoinInformation['name']
+
+# Symbol of instrument
+Symbol = CoinInformation['symbol']
+
+# Description of instrument
+Description = CoinInformation['description']['en']
+
+# Genesis of instrument
+GenesisDate = CoinInformation['genesis_date']
+print('A')

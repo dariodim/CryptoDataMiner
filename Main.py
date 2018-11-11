437e4f47bd3d745d30bbc9f0293255a26311d486
dariodim <k.n.dimitrakopoulos@gmail.com>
2018-11-11 23:34:21 +0100

    Populating Main Table with Multiple Instruments

--- Main.py
+++ Main.py
@@ -1,30 +1,41 @@
 import pandas as pd
+import datetime as datetime
 import Libraries.CoinGecko as CoinGecko_API
 
+# Paramenters
 NumberOfInstruments = 50
 
-# Parse list of coins from CoinGecko by Market Cap (Highest to Lowest)
-CoinList_CALL = 'https://api.coingecko.com/api/v3/coins?order=market_cap_desc?'
+# Sorting
+SortingMethod = 'market_cap_desc'
+
+# Parse list of instruments from CoinGecko by Market Cap (Highest to Lowest)
+CoinList_CALL = 'https://api.coingecko.com/api/v3/coins?order=?' + SortingMethod
 CoinList = pd.DataFrame(CoinGecko_API.CoingeckoAPI(CoinList_CALL).get_coingecko_data())
 
-# Parse full information about bitcoin
-CoinInformation_CALL = 'https://api.coingecko.com//api/v3/coins/' + CoinList['id'][0]
-CoinInformation = CoinGecko_API.CoingeckoAPI(CoinInformation_CALL).get_coingecko_data()
+MainData = pd.DataFrame(columns=['Date', 'Time', 'Id', 'Name', 'Symbol', 'Description', 'GenesisDate'])
 
+for i in range(0,NumberOfInstruments):
 
+    # Parse full information about instrument
+    CoinInformation_CALL = 'https://api.coingecko.com//api/v3/coins/' + CoinList['id'][i]
+    CoinInformation = CoinGecko_API.CoingeckoAPI(CoinInformation_CALL).get_coingecko_data()
 
-# Id of instrument
-Id = CoinInformation['id']
+    # Id of instrument
+    Id = CoinInformation['id']
 
-# Name of instrument
-Name = CoinInformation['name']
+    # Name of instrument
+    Name = CoinInformation['name']
 
-# Symbol of instrument
-Symbol = CoinInformation['symbol']
+    # Symbol of instrument
+    Symbol = CoinInformation['symbol']
 
-# Description of instrument
-Description = CoinInformation['description']['en']
+    # Description of instrument
+    Description = CoinInformation['description']['en']
 
-# Genesis of instrument
-GenesisDate = CoinInformation['genesis_date']
-print('A')
+    # Genesis of instrument
+    GenesisDate = CoinInformation['genesis_date']
+
+    # Add data to Main Table
+    MainData.loc[len(MainData)+1] = [pd.datetime.now().strftime("%Y-%m-%d"), pd.datetime.now().strftime("%H:%M:%S"), Id, Name, Symbol, Description, GenesisDate]
+
+print(MainData)
